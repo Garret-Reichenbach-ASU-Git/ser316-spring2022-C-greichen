1b224d9529281f99bb9c3752f97bba763097d504
TheDerpGamer <garret2kool@gmail.com>
2022-03-01 18:21:51 -0700

    wip work on assignment

--- src/main/java/BearWorkshop.java
+++ src/main/java/BearWorkshop.java
@@ -4,223 +4,243 @@ import java.util.*;
 
 // This class provides functionality for a BearWorkshop class.
 public class BearWorkshop implements main.java.BearWorkshopInterface {
-    // Workshop has a collection of bears
-    // Workshop has a customer
-    main.java.Customer customer;
-    List<main.java.Bear> BearCart;
+	// Workshop has a collection of bears
+	// Workshop has a customer
+	main.java.Customer customer;
+	List<main.java.Bear> BearCart;
 
-    /**
-     * Default constructor for the Bear Workshop
-     */
-    public BearWorkshop() {
-        this("AZ");
-    }
+	/**
+	 * Default constructor for the Bear Workshop
+	 */
+	public BearWorkshop() {
+		this("AZ");
+	}
 
-    /**
-     * This is a parameterized ctor for a BearWorkshop
-     * @param state customer is in
-     */
-    public BearWorkshop(String state) {
-        BearCart = new LinkedList<>();
-        customer = new main.java.Customer(state);
-    }
+	/**
+	 * This is a parameterized ctor for a BearWorkshop
+	 * @param state customer is in
+	 */
+	public BearWorkshop(String state) {
+		BearCart = new LinkedList<>();
+		customer = new main.java.Customer(state);
+	}
 
-    /**
-     * This is a convenience method to calculate the cost of one bears in the
-     * shopping cart for a customer in the BearFactory. It should take the 
-     * accessory discounts into account correctly. 
-     * @param bear to get cost of
-     * @return double representation of bear cost
-     * TODO: test me and fix me in assignment 3
-     */
-    @Override
-    public double getCost(main.java.Bear bear) {
-        Collections.sort(bear.getClothing());
-        int numFree = bear.getClothing().size() / 3;
-        ArrayList<main.java.Clothing> freeClothes = new ArrayList<>();
+	/**
+	 * This is a convenience method to calculate the cost of one bears in the
+	 * shopping cart for a customer in the BearFactory. It should take the
+	 * accessory discounts into account correctly.
+	 * @param bear to get cost of
+	 * @return double representation of bear cost
+	 * TODO: test me and fix me in assignment 3
+	 */
+	@Override
+	public double getCost(main.java.Bear bear) {
+		Collections.sort(bear.getClothing());
+		int numFree = bear.getClothing().size() / 3;
+		ArrayList<main.java.Clothing> freeClothes = new ArrayList<>();
 
-        for (int i = 0; i < bear.getClothing().size(); i++) {
-            main.java.Clothing clothes = bear.getClothing().get(i);
-            if (i < numFree) {
-                freeClothes.add(clothes);
-            } else {
-                bear.setPrice(bear.getPrice() + clothes.price);
-            }
-        }
+		for (int i = 0; i < bear.getClothing().size(); i++) {
+			main.java.Clothing clothes = bear.getClothing().get(i);
+			if (i < numFree) {
+				freeClothes.add(clothes);
+			} else {
+				bear.setPrice(bear.getPrice() + clothes.price);
+			}
+		}
 
-        for (main.java.NoiseMaker noise: bear.getNoisemakers()) {
-            bear.setPrice(bear.getPrice() + noise.price);
-        }
+		for (main.java.NoiseMaker noise: bear.getNoisemakers()) {
+			bear.setPrice(bear.getPrice() + noise.price);
+		}
 
-        if (bear.getInk() != null) {
-            bear.setPrice(bear.getPrice() + bear.getInk().price);
-        }
+		if (bear.getInk() != null) {
+			bear.setPrice(bear.getPrice() + bear.getInk().price);
+		}
 
-        bear.setPrice(bear.getPrice() + bear.getStuff().price);
-        bear.setPrice(bear.getPrice() + bear.getCasing().priceModifier);
+		bear.setPrice(bear.getPrice() + bear.getStuff().price);
+		bear.setPrice(bear.getPrice() + bear.getCasing().priceModifier);
 
-        return bear.getPrice();
-    }
+		return bear.getPrice();
+	}
 
-    // Function to get the raw cost of a bear without any discounts
-   // TODO: test me and fix me in assignment 3
-    public double getRawCost(main.java.Bear bear) {
-        for (int i = 0; i < bear.getClothing().size(); i++) {
-            main.java.Clothing clothes = bear.getClothing().get(i);
-            bear.setPrice(bear.getPrice() + clothes.price);
+	// Function to get the raw cost of a bear without any discounts
+	// TODO: test me and fix me in assignment 3
+	public double getRawCost(main.java.Bear bear) {
+		for (int i = 0; i < bear.getClothing().size(); i++) {
+			main.java.Clothing clothes = bear.getClothing().get(i);
+			bear.setPrice(bear.getPrice() + clothes.price);
 
-        }
+		}
 
-        for (main.java.NoiseMaker noise: bear.getNoisemakers()) {
-            bear.setPrice(bear.getPrice() + noise.price);
-        }
+		for (main.java.NoiseMaker noise: bear.getNoisemakers()) {
+			bear.setPrice(bear.getPrice() + noise.price);
+		}
 
-        if (bear.getInk() != null) {
-            bear.setPrice(bear.getPrice() + bear.getInk().price);
-        }
+		if (bear.getInk() != null) {
+			bear.setPrice(bear.getPrice() + bear.getInk().price);
+		}
 
-        bear.setPrice(bear.getPrice() + bear.getStuff().price);
-        bear.setPrice(bear.getPrice() + bear.getCasing().priceModifier);
+		bear.setPrice(bear.getPrice() + bear.getStuff().price);
+		bear.setPrice(bear.getPrice() + bear.getCasing().priceModifier);
 
-        double bearPrice = bear.getPrice();
-        bear.setPrice(0);
-        return bearPrice;
-    }
+		double bearPrice = bear.getPrice();
+		bear.setPrice(0);
+		return bearPrice;
+	}
 
-    /**
-     * Utility method to calculate tax for purchases by customers in different
-     * states.
-     * You can assume these taxes are what we want, so they are not wrong.
-     * @return
-     */
-    public double calculateTax() {
-        double tax;
-        switch (customer.state) {
-            case "AZ":
-                tax = 1.07;
-                break;
-            case "NY":
-                tax = 1.09;
-                break;
-            case "VA":
-                tax = 1.05;
-                break;
-            case "DC":
-                tax = 1.105;
-                break;
-            case "CA":
-                tax = 1.1;
-                break;
-            default:
-                tax = 1.05;
-                break;
-        }
-        return tax;
-    }
+	/**
+	 * Utility method to calculate tax for purchases by customers in different
+	 * states.
+	 * You can assume these taxes are what we want, so they are not wrong.
+	 * @return
+	 */
+	public double calculateTax() {
+		double tax;
+		switch (customer.state) {
+			case "AZ":
+				tax = 1.07;
+				break;
+			case "NY":
+				tax = 1.09;
+				break;
+			case "VA":
+				tax = 1.05;
+				break;
+			case "DC":
+				tax = 1.105;
+				break;
+			case "CA":
+				tax = 1.1;
+				break;
+			default:
+				tax = 1.05;
+				break;
+		}
+		return tax;
+	}
 
-    /**
-     * Utility method to add a bear to the BearFactory - so ti the shopping cart.
-     * @param bear to add
-     * @return true if successful, false otherwise
-     * TODO: test me and fix me in assignment 3
-     */
-    @Override
-    public boolean addBear(main.java.Bear bear)       {
-        if (this.BearCart.add(bear))        {
-            return true;
-                                            }
-        else                                {
-            return false;
-                                            }
-    }
+	/**
+	 * Utility method to add a bear to the BearFactory - so ti the shopping cart.
+	 * @param bear to add
+	 * @return true if successful, false otherwise
+	 * TODO: test me and fix me in assignment 3
+	 */
+	@Override
+	public boolean addBear(main.java.Bear bear) {
+		return BearCart.add(bear);
+	}
 
-    // Simple means to remove a bear from the shopping cart
-    @Override
-    public boolean removeBear(main.java.Bear bear)    {
-        if (this.BearCart.remove(bear))     {
-            return true;
-                                            }
-        else                                {
-            return false;
-                                            }
-    }
+	// Simple means to remove a bear from the shopping cart
+	@Override
+	public boolean removeBear(main.java.Bear bear) {
+		return BearCart.remove(bear);
+	}
 
-    /**
-     * This is a function to allow customers to checkout their shopping cart
-     * It should return the total cost of they purchase. 
-     * TODO: Test me and fix me in assignment 3
-     * @return
-     */
-    @Override
-    public double checkout() {
-        if (this.customer.age <= 13) {
-            if (this.customer.parent.age < 18) {
-                System.out.println("Guardian is too young");
-                return -1;
-            }
-        }
-        double temp = 0;
-        double Cost = 0;
-        for (main.java.Bear bear: BearCart) {
-            Cost = Cost + getRawCost(bear);
-        }
-        for (main.java.Bear bear: this.BearCart) {
-            temp += getCost(bear);
-        }
+	/**
+	 * This is a function to allow customers to checkout their shopping cart
+	 * It should return the total cost of they purchase.
+	 * TODO: Test me and fix me in assignment 3
+	 * @return
+	 */
+	@Override
+	public double checkout() {
+		if (this.customer.age <= 13) {
+			if (this.customer.parent.age < 18) {
+				System.out.println("Guardian is too young");
+				return -1;
+			}
+		}
+		double temp = 0;
+		double Cost = 0;
+		for (main.java.Bear bear: BearCart) {
+			Cost = Cost + getRawCost(bear);
+		}
+		for (main.java.Bear bear: this.BearCart) {
+			temp += getCost(bear);
+		}
 
 
-        double savings = 0;
-        // calculate total cost
-        double rawCost = 0;
-        for (main.java.Bear bear: BearCart) {
-            rawCost += this.getRawCost(bear);
-        }
+		double savings = 0;
+		// calculate total cost
+		double rawCost = 0;
+		for (main.java.Bear bear: BearCart) {
+			rawCost += this.getRawCost(bear);
+		}
 
-        // calculate adjusted cost
-        double cost = 0;
-        for (main.java.Bear bear: this.BearCart) {
-            cost += this.getCost(bear);
-        }
-        savings += rawCost - cost; // calc delta between raw and prorated cost
+		// calculate adjusted cost
+		double cost = 0;
+		for (main.java.Bear bear: this.BearCart) {
+			cost += this.getCost(bear);
+		}
+		savings += rawCost - cost; // calc delta between raw and prorated cost
 
-        List<main.java.Bear> nonFreeBears = new LinkedList<>();
-        int counter = 0;
-        int numberOfFreeBearsInBearCart = BearCart.size() / 3;
-        double discountedCost = 0;
-        main.java.Bear freeBear = null;
+		List<main.java.Bear> nonFreeBears = new LinkedList<>();
+		int counter = 0;
+		int numberOfFreeBearsInBearCart = BearCart.size() / 3;
+		double discountedCost = 0;
+		main.java.Bear freeBear = null;
 
-        for (int count = 0; count <= numberOfFreeBearsInBearCart; ++count) {
-            for (main.java.Bear bear : BearCart) {
-                if (freeBear != null && bear.getPrice() < freeBear.getPrice())
-                    freeBear = bear;
-                    temp += temp - temp * 2 + bear.getPrice();
+		for (int count = 0; count <= numberOfFreeBearsInBearCart; ++count) {
+			for (main.java.Bear bear : BearCart) {
+				if (freeBear != null && bear.getPrice() < freeBear.getPrice())
+					freeBear = bear;
+				temp += temp - temp * 2 + bear.getPrice();
 
-            }
-        }
-        cost = temp;
+			}
+		}
+		cost = temp;
 
-        return cost * calculateTax();
-    }
+		return cost * calculateTax();
+	}
 
 
-    /**
-     * This method returns the savings for advertised bundle savings.
-     * Specifically, 
-     * - Bears are Buy 2 bears, get a third one free. It is always the cheapest bear that is free. The price here is meant when all discounts for a single bear are applied
-     * - It is 10% off the cost of a bear when a single bear has 10 or more accessories (anything on a bear is an accessory) that the customer pays for (so if clothes are free these do not count). 
-     * - Clothes are buy 2, get one free on each bear. Always the cheapest clothes are free. So if a bear has 6 clothes, then the two cheapest ones will be free and it would count as 4 accessories (see above).
-     * - Inking on a specific bear is free if and only if the bear without discounts applied to it costs more than $70. 
-     * TIP: the implemented savings method in the BearWorkshop1-5 do not use the getCost method implemented in this base class. They implement their own savings calculation
-     *  		 All of them do however use the getRawCost method implemented in this base class. 
-     * EXAMPLE: You buy 3 bears, one bear has 3 clothing items, the other two have 4 clothing items. Non of them have embroidery or noise makers and they have the same stuffing. 
-     * Now, on each bear one clothing item will be free, since buy 2 get 1 free on a bear. So for costs we have the bear with stuffing. For one we pay only 2 clothing items, for 2 we still pay for 3 clothing items. 
-     * Since all clothing is the same priece the bear with only 2 paid clothing items is cheapest. So we will get that bear for free. We will only have to pay for 2 bears, with 3 clothing items each. 
-     * @return the savings if the customer would check with the current bears in the workshop out as double
-     */
-    public double calculateSavings() {
-        System.out.println("TODO: Implement me in Assignment 3");
-        return 0.0;
-    }
-    
- 
+	/**
+	 * This method returns the savings for advertised bundle savings.
+	 * Specifically,
+	 * - Bears are Buy 2 bears, get a third one free. It is always the cheapest bear that is free. The price here is meant when all discounts for a single bear are applied
+	 * - It is 10% off the cost of a bear when a single bear has 10 or more accessories (anything on a bear is an accessory) that the customer pays for (so if clothes are free these do not count).
+	 * - Clothes are buy 2, get one free on each bear. Always the cheapest clothes are free. So if a bear has 6 clothes, then the two cheapest ones will be free and it would count as 4 accessories (see above).
+	 * - Inking on a specific bear is free if and only if the bear without discounts applied to it costs more than $70.
+	 * TIP: the implemented savings method in the BearWorkshop1-5 do not use the getCost method implemented in this base class. They implement their own savings calculation
+	 *  		 All of them do however use the getRawCost method implemented in this base class.
+	 * EXAMPLE: You buy 3 bears, one bear has 3 clothing items, the other two have 4 clothing items. Non of them have embroidery or noise makers and they have the same stuffing.
+	 * Now, on each bear one clothing item will be free, since buy 2 get 1 free on a bear. So for costs we have the bear with stuffing. For one we pay only 2 clothing items, for 2 we still pay for 3 clothing items.
+	 * Since all clothing is the same priece the bear with only 2 paid clothing items is cheapest. So we will get that bear for free. We will only have to pay for 2 bears, with 3 clothing items each.
+	 * @return the savings if the customer would check with the current bears in the workshop out as double
+	 */
+	public double calculateSavings() {
+		double savings = 0;
+		double rawCost = 0;
+		double cost = 0;
+
+		Collections.sort(BearCart);
+		HashMap<main.java.Bear, Boolean> bears = new HashMap<>();
+		ArrayList<main.java.Bear> tempList = new ArrayList<>();
+
+		for(main.java.Bear bear : BearCart) {
+			bears.remove(bear);
+			tempList.add(bear);
+			Collections.sort(tempList);
+			if(tempList.size() == 3) {
+				bears.remove(tempList.get(0));
+				bears.remove(tempList.get(1));
+				bears.remove(tempList.get(2));
+				bears.put(tempList.get(0), false);
+				bears.put(tempList.get(1), false);
+				bears.put(tempList.get(2), true);
+				tempList.clear();
+			} else bears.put(bear, false);
+		}
+
+		for(Map.Entry<main.java.Bear, Boolean> entry : bears.entrySet()) {
+			if(entry.getValue()) {
+				
+			} else {
+				rawCost += getRawCost(entry.getKey());
+				cost = getCost(entry.getKey());
+			}
+		}
+		savings += rawCost - cost;
+
+		return savings;
+	}
 }
